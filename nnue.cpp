142fe691293227c4c63c66564946f9b498eb5687
Chris Bao <89361988+sscg13@users.noreply.github.com>
2024-07-14 17:06:27 -0700

    Revamp NNUE parsing, get incbin to work

--- nnue.cpp
+++ nnue.cpp
@@ -1,6 +1,11 @@
 #include <cmath>
 #include <fstream>
+#define INCBIN_PREFIX
+#include "incbin.h"
+
 const int nnuesize = 64;
+const int nnuefilesize = 1542 * nnuesize + 2;
+INCBIN(char, NNUE, EUNNfile);
 int screlu(short int x) {
   return std::pow(std::max(std::min((int)x, 255), 0), 2);
 }
@@ -17,6 +22,7 @@ class NNUE {
   int evalQB = 64;
 
 public:
+  void loaddefaultnet();
   void readnnuefile(std::string file);
   void activatepiece(int color, int piece, int square);
   void deactivatepiece(int color, int piece, int square);
@@ -26,52 +32,80 @@ public:
   int evaluate(int color);
 };
 
-void NNUE::readnnuefile(std::string file) {
-  std::ifstream nnueweights;
-  nnueweights.open(file, std::ifstream::binary);
+void NNUE::loaddefaultnet() {
+  int offset = 0;
   for (int i = 0; i < 768; i++) {
     int piece = i / 64;
     int square = i % 64;
     int convert[12] = {0, 3, 1, 4, 2, 5, 6, 9, 7, 10, 8, 11};
     for (int j = 0; j < nnuesize; j++) {
-      char *weights = new char[2];
-      nnueweights.read(weights, 2);
-      short int weight = 256 * (short int)(weights[1]) +
-                         (short int)(unsigned char)(weights[0]);
+      short int weight = 256 * (short int)(NNUEData[offset + 1]) +
+                         (short int)(unsigned char)(NNUEData[offset]);
       nnuelayer1[64 * convert[piece] + square][j] = weight;
-      delete[] weights;
+      offset += 2;
     }
   }
   for (int i = 0; i < nnuesize; i++) {
-    char *biases = new char[2];
-    nnueweights.read(biases, 2);
-    short int bias =
-        256 * (short int)(biases[1]) + (short int)(unsigned char)(biases[0]);
+    short int bias = 256 * (short int)(NNUEData[offset + 1]) +
+                     (short int)(unsigned char)(NNUEData[offset]);
     layer1bias[i] = bias;
-    delete[] biases;
+    offset += 2;
   }
   for (int i = 0; i < nnuesize; i++) {
-    char *weights = new char[2];
-    nnueweights.read(weights, 2);
-    short int weight =
-        256 * (short int)(weights[1]) + (short int)(unsigned char)(weights[0]);
+    short int weight = 256 * (short int)(NNUEData[offset + 1]) +
+                       (short int)(unsigned char)(NNUEData[offset]);
     ourlayer2[i] = (int)weight;
-    delete[] weights;
+    offset += 2;
   }
   for (int i = 0; i < nnuesize; i++) {
-    char *weights = new char[2];
-    nnueweights.read(weights, 2);
-    short int weight =
-        256 * (short int)(weights[1]) + (short int)(unsigned char)(weights[0]);
+    short int weight = 256 * (short int)(NNUEData[offset + 1]) +
+                       (short int)(unsigned char)(NNUEData[offset]);
     theirlayer2[i] = (int)weight;
-    delete[] weights;
+    offset += 2;
   }
-  char *bases = new char[2];
-  nnueweights.read(bases, 2);
-  short int base =
-      256 * (short int)(bases[1]) + (short int)(unsigned char)(bases[0]);
+  short int base = 256 * (short int)(NNUEData[offset + 1]) +
+                   (short int)(unsigned char)(NNUEData[offset]);
   finalbias = base;
-  delete[] bases;
+}
+void NNUE::readnnuefile(std::string file) {
+  std::ifstream nnueweights;
+  nnueweights.open(file, std::ifstream::binary);
+  char *weights = new char[nnuefilesize];
+  nnueweights.read(weights, nnuefilesize);
+  int offset = 0;
+  for (int i = 0; i < 768; i++) {
+    int piece = i / 64;
+    int square = i % 64;
+    int convert[12] = {0, 3, 1, 4, 2, 5, 6, 9, 7, 10, 8, 11};
+    for (int j = 0; j < nnuesize; j++) {
+      short int weight = 256 * (short int)(weights[offset + 1]) +
+                         (short int)(unsigned char)(weights[offset]);
+      nnuelayer1[64 * convert[piece] + square][j] = weight;
+      offset += 2;
+    }
+  }
+  for (int i = 0; i < nnuesize; i++) {
+    short int bias = 256 * (short int)(weights[offset + 1]) +
+                     (short int)(unsigned char)(weights[offset]);
+    layer1bias[i] = bias;
+    offset += 2;
+  }
+  for (int i = 0; i < nnuesize; i++) {
+    short int weight = 256 * (short int)(weights[offset + 1]) +
+                       (short int)(unsigned char)(weights[offset]);
+    ourlayer2[i] = (int)weight;
+    offset += 2;
+  }
+  for (int i = 0; i < nnuesize; i++) {
+    short int weight = 256 * (short int)(weights[offset + 1]) +
+                       (short int)(unsigned char)(weights[offset]);
+    theirlayer2[i] = (int)weight;
+    offset += 2;
+  }
+  short int base = 256 * (short int)(weights[offset + 1]) +
+                   (short int)(unsigned char)(weights[offset]);
+  finalbias = base;
+  delete[] weights;
   nnueweights.close();
 }
 void NNUE::activatepiece(int color, int piece, int square) {
